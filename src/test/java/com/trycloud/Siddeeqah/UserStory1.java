fc34b214622c78ecc04e0c982755dd512a208313
Siddeeqah Sharif Fichman <siddeeqah.sharif@gmail.com>
2021-02-05 13:15:51 -0500

    Second commit Sid

--- src/test/java/com/trycloud/Siddeeqah/UserStory1.java
+++ src/test/java/com/trycloud/Siddeeqah/UserStory1.java
@@ -1,8 +1,32 @@
 package com.trycloud.Siddeeqah;
 
+import com.trycloud.utilities.WebDriverFactory;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
 public class UserStory1 {
 
     public static void main(String[] args) {
-        System.out.println("================");
+
+        WebDriver driver = WebDriverFactory.getDriver("chrome");
+        driver.get("http://qa.trycloud.net/index.php/login?clear=1");
+
+        WebElement userNameBlock = driver.findElement(By.xpath("//input[@id='user']"));
+        WebElement passwordBlock = driver.findElement(By.xpath("//input[@id='password']"));
+        WebElement loginBlock = driver.findElement(By.xpath("//input[@id='submit-form']"));
+        userNameBlock.sendKeys("User10");
+        passwordBlock.sendKeys("Userpass123");
+        loginBlock.click();
+        //figure out verification
+
+        userNameBlock.sendKeys("FakeName");
+        passwordBlock.sendKeys("FakePassword");
+        loginBlock.click();
+
+
+
+
+
     }
 }
